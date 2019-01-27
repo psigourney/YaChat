636913a73dc662875b54f2ffdd15fd7fb566485e
Patrick Sigourney <psigourney@gmail.com>
2019-01-27 06:23:12 -0600

    Created client

---
+++ client_YaChat.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+################################################################
+# YaChat - Yet Another Chat Program
+#
+# Patrick Sigourney - pws482
+# University of Texas at Austin - Spring 2019
+# EE382N - Networks; Dr. Yerraballi
+#
+# Usage: client.py <screen_name> <server_host> <server_port>
+#
+################################################################
+
+
